cad107f79ae74b11e8487a78985a52dd87e9eec5
me-lw-in <melwinmanish30@gmail.com>
2025-04-30 21:09:09 +0530

    Update code

--- src/main/java/com/crime/reporting_system/ReportingSystemApplication.java
+++ src/main/java/com/crime/reporting_system/ReportingSystemApplication.java
@@ -1,5 +1,6 @@
 package com.crime.reporting_system;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,6 +10,9 @@ import org.springframework.context.annotation.ComponentScan;
 public class ReportingSystemApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load(); // Load .env file
+		System.out.println("DB_HOST: " + dotenv.get("DB_HOST"));
+		System.out.println("DB_PORT: " + dotenv.get("DB_PORT"));
 		SpringApplication.run(ReportingSystemApplication.class, args);
 		System.out.println("hello main");
 	}
