448454b3c799830164d0cb552b3c7770f0f4b723
me-lw-in <melwinmanish30@gmail.com>
2025-05-02 22:05:43 +0530

    Removed import of Dotenv from main

--- src/main/java/com/crime/reporting_system/ReportingSystemApplication.java
+++ src/main/java/com/crime/reporting_system/ReportingSystemApplication.java
@@ -1,6 +1,5 @@
 package com.crime.reporting_system;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
